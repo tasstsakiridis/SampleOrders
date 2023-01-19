f0e0791e288e56c3574e87a0cec9734879d3ee7f
Tass Tsakiridis <tasstsakiridis@gmail.com>
2023-01-20 08:23:11 +1100

    Remove Duplicate KR country code

--- force-app/main/default/aura/SampleOrderForm/SampleOrderFormHelper.js
+++ force-app/main/default/aura/SampleOrderForm/SampleOrderFormHelper.js
@@ -38,9 +38,6 @@
         KR: [
             {label:'Seoul', value:'Seoul'},
         ]
-        KR: [
-            {label:'Seoul', value:'Seoul'},
-        ]
     },
     countryOptions: [
         {'label': 'Australia', 'value': 'AU'},
